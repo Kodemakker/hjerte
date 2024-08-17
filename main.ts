31950512382f21a04f8de10af2b327ceb7522548
Kodemakker <andershess.ahrensbach@gmail.com>
2024-08-17 15:47:17 +0200

    De første filer til MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.Diamond)
+    basic.pause(100)
+    basic.showIcon(IconNames.House)
+    basic.pause(100)
+    basic.showIcon(IconNames.Confused)
+    basic.pause(100)
+})
